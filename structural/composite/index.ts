963fc81a90ce5d8a125d903f7eb851bd7395ab26
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-01-04 23:54:55 +0700

    composite

---
+++ structural/composite/index.ts
@@ -0,0 +1,22 @@
+class Category {
+    name: string;
+    children: any[] = [];
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const computer = new Category("Computer");
+
+computer.children = [
+    new Category("PC").children = [
+        new Category("Case"),
+        new Category("Motherboard"),
+    ],
+    new Category("Laptop").children = [
+        new Category("Asus ROG")
+    ]
+];
+
+console.log(computer);
